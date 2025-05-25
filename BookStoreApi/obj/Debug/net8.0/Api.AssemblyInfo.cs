d0d2552a103d7d31bae171cecc9ed1392de84b8e
Noam <noambar2845@gmail.com>
2025-05-25 03:27:57 +0300

    docker file name

--- BookStoreApi/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ BookStoreApi/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d03d3fd59e34587e659d5c12cd017418782f6382")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3bf7e7e11323f517fc1dd4d4557a4eeab102c65")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
